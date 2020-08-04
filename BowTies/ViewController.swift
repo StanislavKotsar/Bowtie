3326d3c6879540e6f34556d789f7e8474a4b7379
Stanislav Kotsar <minolin27@gmail.com>
2020-08-04 20:50:41 +0300

    add validation

--- BowTies/ViewController.swift
+++ BowTies/ViewController.swift
@@ -76,7 +76,24 @@ class ViewController: UIViewController {
   // MARK: - IBActions
 
   @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+    guard let selectedValue = sender.titleForSegment(
+      at: sender.selectedSegmentIndex) else {
+        return
+    }
 
+    let request: NSFetchRequest<BowTie> = BowTie.fetchRequest()
+    request.predicate = NSPredicate(
+      format: "%K = %@",
+      argumentArray: [#keyPath(BowTie.searchKey), selectedValue])
+
+    do {
+      let results =  try managedContext.fetch(request)
+      currentBowTie =  results.first
+      populate(bowtie: currentBowTie)
+
+    } catch let error as NSError {
+      print("Could not fetch \(error), \(error.userInfo)")
+    }
   }
 
   @IBAction func wear(_ sender: UIButton) {
@@ -128,11 +145,20 @@ class ViewController: UIViewController {
     }
 
     do {
+
       currentBowTie.rating = rating
       try managedContext.save()
       populate(bowtie: currentBowTie)
+
     } catch let error as NSError {
-      print("Could not save \(error), \(error.userInfo)")
+
+      if error.domain == NSCocoaErrorDomain &&
+        (error.code == NSValidationNumberTooLargeError ||
+          error.code == NSValidationNumberTooSmallError) {
+        rate(rateButton)
+      } else {
+        print("Could not save \(error), \(error.userInfo)")
+      }
     }
   }
 
